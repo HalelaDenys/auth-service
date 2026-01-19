db85bfd7dd9043a1ab9564eef50e88167c2122c7
_Avvake_ <denisergo18@gmail.com>
2026-01-19 18:21:39 +0200

    updated the config file

---
+++ src/core/config.py
@@ -0,0 +1,57 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic import BaseModel
+from typing import ClassVar
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+
+
+class MiddlewareConfig(BaseModel):
+    cors_allowed_origins: list[str]
+
+class DBConfig(BaseModel):
+    naming_convention: ClassVar[dict] = {
+        "ix": "ix_%(column_0_label)s",
+        "uq": "uq_%(table_name)s_%(column_0_N_name)s",
+        "ck": "ck_%(table_name)s_%(constraint_name)s",
+        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+        "pk": "pk_%(table_name)s",
+    }
+
+    postgres_user: str
+    postgres_password: str
+    postgres_host: str
+    postgres_port: int
+    postgres_db: str
+    echo: bool = False
+
+    @property
+    def dsn(self) -> str:
+        return (
+            f"postgresql+asyncpg://{self.postgres_user}:{self.postgres_password}@"
+            f"{self.postgres_host}:{self.postgres_port}/{self.postgres_db}"
+        )
+
+class AuthConfig(BaseModel):
+    secret_key: str
+    access_expire_day: int
+    refresh_expire_day: int
+    algorithm: str = "HS256"
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=BASE_DIR / ".env",
+        env_prefix="APP_CONFIG__",
+        env_nested_delimiter="__",
+        case_sensitive=False,
+    )
+
+    db: DBConfig
+    midd: MiddlewareConfig
+    jwt: AuthConfig
+    mode: str
+
+
+settings = Settings()
+
