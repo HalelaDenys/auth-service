3d92b926f2e0e40ce4ea109bcc50bb7b0e243461
_Avvake_ <denisergo18@gmail.com>
2026-01-24 18:33:39 +0200

    added new model refresh_token

--- src/infrastructure/__init__.py
+++ src/infrastructure/__init__.py
@@ -2,8 +2,10 @@ __all__ = [
     "db_helper",
     "Base",
     "User",
+    "RefreshToken",
 ]
 
 from infrastructure.db.db_helper import db_helper
 from infrastructure.db.models.base import Base
 from infrastructure.db.models.users import User
+from infrastructure.db.models.refresh_token import RefreshToken
