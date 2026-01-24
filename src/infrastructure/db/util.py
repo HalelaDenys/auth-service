f40f19d0257b431d05e01041f38c1caf0e6dac05
_Avvake_ <denisergo18@gmail.com>
2026-01-24 18:33:45 +0200

    added new model refresh_token

---
+++ src/infrastructure/db/util.py
@@ -0,0 +1,10 @@
+import re
+
+
+def camel_to_snake(name: str) -> str:
+    """
+    CamelCase -> snake_case.
+    """
+    s1 = re.sub(r"(.)([A-Z][a-z]+)", r"\1_\2", name)
+    s2 = re.sub(r"([a-z0-9])([A-Z])", r"\1_\2", s1)
+    return s2.lower()
